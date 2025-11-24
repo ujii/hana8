f5b8634e4589a9638d65598264603b257c373642
ujii <wcm9763@naver.com>
2025-11-24 14:29:41 +0900

    연습문제 312p

--- js/trythis/getPosts.js
+++ js/trythis/getPosts.js
@@ -1,27 +1,31 @@
-async function getPosts(id) {
-  const postsResponse = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
-  const posts = await postsResponse.json();
+// 연습문제 312p
 
-  const promiseAll = (posts) =>
-    Promise.all((resolve) => {
-      const comments = [];
-      posts.map(async post => )
-    });
+const API = 'https://jsonplaceholder.typicode.com';
+const getPostsUserId = async (userId) =>
+  fetch(`${API}/posts?userId=${userId}`).then((res) => res.json());
+const getCommentByPostId = async (postId) =>
+  fetch(`${API}/posts/${postId}/comments`).then((res) => res.json());
+
+async function fetchData() {
+  const posts = await getPostsUserId(1);
+  // console.log('🚀 ~ posts:', posts);
+
+  const postComments = await Promise.all(
+    posts.map((post) => getCommentByPostId(post.id))
+  );
+
+  const results = [];
+  for (let i = 0; i < posts.length; i++) {
+    const { id: postId, title } = posts[i];
+    const comments = postComments[i].map(({ id, email, body }) => ({
+      id,
+      email,
+      body,
+    }));
+    results.push({ postId, title, comments });
+  }
+
+  console.log('🚀 ~ results:', JSON.stringify(results, null, ' '));
 }
 
-getPosts(1);
-/*
-[
-    {
-        postId: 게시글ID,
-        title: 게시글 제목,
-        comments: [댓글 목록]
-    },
-    { … }
-]
-const commentsResponse = await fetch(
-  'https://jsonplaceholder.typicode.com/posts/<postId>/comments'
-);
-*/
+fetchData();
