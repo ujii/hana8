766f42de6e9f580b29ba174ccef2154d0feccd70
ujii <wcm9763@naver.com>
2025-12-16 09:10:46 +0900

    리액트 연습문제 45p

--- react/rbvite/src/hooks/SessionContext.tsx
+++ react/rbvite/src/hooks/SessionContext.tsx
@@ -5,6 +5,7 @@ import {
   useState,
   createContext,
   useRef,
+  useReducer,
 } from 'react';
 import type { LoginHandler } from '../components/Login';
 
@@ -51,67 +52,123 @@ const SessionContext = createContext<SessionContextValue>({
   saveItem: () => {},
 });
 
+type Action =
+  | { type: 'login'; payload: LoginUser }
+  | { type: 'logout'; payload: null }
+  | { type: 'removeItem'; payload: number }
+  | { type: 'saveItem'; payload: ItemType };
+
+const reducer = (session: Session, action: Action) => {
+  switch (action.type) {
+    case 'login':
+      return { ...session, loginUser: action.payload };
+    case 'logout':
+      return { ...session, loginUser: null };
+    case 'removeItem':
+      return {
+        ...session,
+        cart: session.cart.filter((item) => item.id !== action.payload),
+      };
+    case 'saveItem':
+      const item =
+        action.payload.id &&
+        session.cart.find((item) => item.id === action.payload.id);
+
+      if (!item) {
+        const newItem = {
+          id: Math.max(...session.cart.map((item) => item.id), 0) + 1,
+          name: action.payload.name,
+          price: action.payload.price,
+        };
+        return { ...session, cart: [...session.cart, newItem] };
+      } else {
+        return {
+          ...session,
+          cart: session.cart.map((item) =>
+            item.id === action.payload.id
+              ? {
+                  ...item,
+                  name: action.payload.name,
+                  price: action.payload.price,
+                }
+              : item
+          ),
+        };
+      }
+    default:
+      return session;
+  }
+};
+
 export function SessionProvider({ children }: PropsWithChildren) {
-  const [session, setSession] = useState<Session>(DefaultSession);
+  //   const [session, setSession] = useState<Session>(DefaultSession);
+  const [session, dispatch] = useReducer(reducer, {} as Session);
 
   const loginHandlerRef = useRef<LoginHandler | null>(null);
 
-  const logout = () => {
-    // session.loginUser = null;
-    setSession({ ...session, loginUser: null });
-  };
+  //   const logout = () => {
+  //     // session.loginUser = null;
+  //     setSession({ ...session, loginUser: null });
+  //   };
+  const logout = () => dispatch({ type: 'logout', payload: null });
 
-  const login: LoginFunction = (name, age) => {
-    // if (!name || !age) return alert('Input Name and Age');
-    if (loginHandlerRef.current?.validate())
-      setSession({ ...session, loginUser: { id: 1, name, age } });
-  };
+  //   const login: LoginFunction = (name, age) => {
+  //     // if (!name || !age) return alert('Input Name and Age');
+  //     if (loginHandlerRef.current?.validate())
+  //       setSession({ ...session, loginUser: { id: 1, name, age } });
+  //   };
+  const login = (name: string, age: number) =>
+    dispatch({ type: 'login', payload: { id: 1, name, age } });
 
-  const removeItem = (id: number) => {
-    if (!confirm('Are you sure?')) return;
+  //   const removeItem = (id: number) => {
+  //     if (!confirm('Are you sure?')) return;
 
-    // 새 배열 두번 반환. 좋은 코드 x
-    // setSession({
-    //   ...session,
-    //   cart: [...session.cart.filter((ItemTypes) => ItemTypes.id !== id)],
-    // });
+  //     // 새 배열 두번 반환. 좋은 코드 x
+  //     // setSession({
+  //     //   ...session,
+  //     //   cart: [...session.cart.filter((ItemTypes) => ItemTypes.id !== id)],
+  //     // });
 
-    // 좋은 코드
-    setSession({
-      ...session,
-      cart: session.cart.filter((item) => item.id !== id),
-    });
-  };
+  //     // 좋은 코드
+  //     setSession({
+  //       ...session,
+  //       cart: session.cart.filter((item) => item.id !== id),
+  //     });
+  //   };
+  const removeItem = (id: number) =>
+    dispatch({ type: 'removeItem', payload: id });
 
-  const saveItem = ({ id, name, price }: ItemType) => {
-    // item이 카트에 존재해야지만 수정 가능
-    // if(id) {} 코드는 보안 문제로 권장 x
-    const item = id && session.cart.find((item) => item.id === id);
-    if (item) {
-      // item.name = name;
-      // item.price = price;
-      setSession({
-        ...session,
-        cart: session.cart.map((item) =>
-          item.id === id ? { id, name, price } : item
-        ),
-      });
-    } else {
-      const newItem = {
-        id: Math.max(...session.cart.map((ItemTypes) => ItemTypes.id), 0) + 1,
-        name,
-        price,
-      };
-      // session.cart.push(newItem);
-      setSession({ ...session, cart: [...session.cart, newItem] });
-    }
-    // setSession({ ...session, cart: [...session.cart] });
+  //   const saveItem = ({ id, name, price }: ItemType) => {
+  //     // item이 카트에 존재해야지만 수정 가능
+  //     // if(id) {} 코드는 보안 문제로 권장 x
+  //     const item = id && session.cart.find((item) => item.id === id);
+  //     if (item) {
+  //       // item.name = name;
+  //       // item.price = price;
+  //       setSession({
+  //         ...session,
+  //         cart: session.cart.map((item) =>
+  //           item.id === id ? { id, name, price } : item
+  //         ),
+  //       });
+  //     } else {
+  //       const newItem = {
+  //         id: Math.max(...session.cart.map((ItemTypes) => ItemTypes.id), 0) + 1,
+  //         name,
+  //         price,
+  //       };
+  //       // session.cart.push(newItem);
+  //       setSession({ ...session, cart: [...session.cart, newItem] });
+  //     }
+  //     // setSession({ ...session, cart: [...session.cart] });
 
-    // 결과는 아래 코드와 같지만 cart가 re rendering 되는 것이 아니고 session에 의해 렌더링됨. 잘못된 코드.
-    // session.cart.push(newItem);
-    // setSession([...session]);
-    // setSession({ ...session, cart: [...session.cart, newItem] });
-  };
+  //     // 결과는 아래 코드와 같지만 cart가 re rendering 되는 것이 아니고 session에 의해 렌더링됨. 잘못된 코드.
+  //     // session.cart.push(newItem);
+  //     // setSession([...session]);
+  //     // setSession({ ...session, cart: [...session.cart, newItem] });
+  //   };
+  const saveItem = ({ id, name, price }: ItemType) =>
+    dispatch({ type: 'saveItem', payload: { id, name, price } });
 
   return (
     <SessionContext.Provider
