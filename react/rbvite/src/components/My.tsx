68505a4f4c786d26307a3b5d18aa48681a4fcd01
ujii <wcm9763@naver.com>
2025-12-16 14:52:06 +0900

    useEffect & useLayoutEffect

--- react/rbvite/src/components/My.tsx
+++ react/rbvite/src/components/My.tsx
@@ -9,7 +9,14 @@ import Profile, { type ProfileHandler } from './Profile';
 import Item from './Item';
 import Button from './ui/Button';
 import { PlusIcon } from 'lucide-react';
-import { useEffect, useRef, useState, type RefObject, useReducer } from 'react';
+import {
+  useEffect,
+  useRef,
+  useState,
+  type RefObject,
+  useReducer,
+  useLayoutEffect,
+} from 'react';
 import { useInterval } from '../hooks/interval';
 
 export default function My() {
@@ -46,16 +53,31 @@ export default function My() {
 
   // goodSec + 1 의 값이
   console.log('🚀 ~ goodSec:', goodSec);
-  useInterval(ff, 1000, goodSec + 1);
+  const { reset, clear } = useInterval(ff, 1000, goodSec + 1);
   // useInterval(setGoodSec, 1000, goodSec + 1);
   // useInterval(() => setGoodSec((p) => p + 1), 1000);
   // useInterval(f, 1000);
+
+  const [data, setData] = useState<ItemType[]>([]);
+  useLayoutEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    fetch('/data/sample.json', { signal })
+      .then((res) => res.json())
+      .then(setData);
+
+    return () => controller.abort();
+  }, []);
 
   return (
     <>
       <h1 className='text-xl'>
         bad: {badSec}, good: {goodSec}
       </h1>
+      <div className='flex'>
+        <button onClick={reset}>reset</button>
+        <button onClick={clear}>clear</button>
+      </div>
       {session?.loginUser ? <Profile ref={profileHandlerRef} /> : <Login />}
       <hr />
       <a
@@ -69,7 +91,7 @@ export default function My() {
         {item101?.name}
       </a>
       <ul>
-        {session.cart.map((item) => (
+        {data.map((item) => (
           <li key={item.id}>
             <Item item={item} />
           </li>
--- react/rbvite/src/hooks/interval.ts
+++ react/rbvite/src/hooks/interval.ts
@@ -1,9 +1,9 @@
 // useInterval(() => setGoodSec((p) => p + 1, 1000);
 
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // useInterval(console.log, 1000, x, y, z);
-export function useInterval<T extends (...args: Parameters<T>) => void>(
+export function useInterval_OLD<T extends (...args: Parameters<T>) => void>(
   cb: T,
   delay: number,
   ...args: Parameters<T>
@@ -23,14 +23,85 @@ export function useInterval<T extends (...args: Parameters<T>) => void>(
   }, []);
 }
 
+function time<T extends () => void>(
+  f: typeof setTimeout | typeof setInterval,
+  cb: T,
+  delay: number,
+  ...args: Parameters<T>
+) {
+  // const [timer, setTimer] = useState<ReturnType<typeof f>>();
+  const timerRef = useRef<ReturnType<typeof f>>(undefined);
+
+  const setTime = () => {
+    timerRef.current = f(cb, delay, ...args);
+  };
+  // const clear = () =>
+  //   f === setTimeout
+  //     ? clearTimeout(timerRef.current)
+  //     : clearInterval(timerRef.current);
+  const clear = () =>
+    (f === setTimeout ? clearTimeout : clearInterval)(timerRef.current);
+
+  const reset = () => {
+    clear();
+    setTime();
+  };
+
+  useEffect(() => {
+    setTime();
+
+    return clear;
+  }, []);
+
+  return { clear, reset };
+}
+function time_OLD<T extends () => void>(
+  f: typeof setTimeout | typeof setInterval,
+  cb: T,
+  delay: number,
+  ...args: Parameters<T>
+) {
+  const [timer, setTimer] = useState<ReturnType<typeof f>>();
+
+  const setTime = () => {
+    const timer = f(cb, delay, ...args);
+    setTimer(timer);
+    return timer;
+  };
+  const clear = (t?: ReturnType<typeof f>) =>
+    f === setTimeout ? clearTimeout(t || timer) : clearInterval(t || timer);
+  const reset = () => {
+    clear();
+    // setTimer(f(cb, delay, ...args));
+    setTime();
+  };
+
+  useEffect(() => {
+    // const timer = f(cb, delay, ...args);
+    // setTimer(timer);
+
+    // setTimer(f(cb, delay, ...args));
+    const timer = setTime();
+
+    // return () => clearTimeout(timer);
+    return () => clear(timer);
+  }, []);
+
+  return { clear, reset };
+}
+
+export function useInterval<T extends (...args: Parameters<T>) => void>(
+  cb: T,
+  delay: number,
+  ...args: Parameters<T>
+) {
+  return time(setInterval, cb, delay, ...args);
+}
+
 export function useTimeout<T extends () => void>(
   cb: T,
   delay: number,
   ...args: Parameters<T>
 ) {
-  useEffect(() => {
-    const timer = setTimeout(cb, delay, ...args);
-
-    return () => clearTimeout(timer);
-  }, []);
+  return time(setTimeout, cb, delay, ...args);
 }
