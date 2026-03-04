1add16d0de68043e359d800a0fb43da0f3c12bf7
ujii <wcm9763@naver.com>
2026-03-04 13:51:23 +0900

    H2DB ERROR

--- java/demo/src/test/java/com/hana8/demo/repository/MemberRepositoryTest.java
+++ java/demo/src/test/java/com/hana8/demo/repository/MemberRepositoryTest.java
@@ -20,7 +20,7 @@ import com.hana8.demo.entity.Member;
 
 @DataJpaTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // H2DB를 안 쓸 때 이 어노테이션 있어야 에러 x
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Rollback(false)
 class MemberRepositoryTest {
